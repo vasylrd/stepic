68da1116ae76802c7f05c5ee7a39d52febc48841
vasylrd <vasyl.rd@gmail.com>
2016-12-02 13:37:30 +0200

    Update hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 
-def application(env, start_response):
+def app(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
     result = '\n'.join(env['QUERY_STRING'].split('&'))
     return [result]
