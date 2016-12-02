9c2e07f5cef33d3cfb74eb2e09d4b18c252a214a
vasylrd <vasyl.rd@gmail.com>
2016-12-02 15:02:29 +0200

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+
+def app(env, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return ['\n'.join(env['QUERY_STRING'].split('&'))]
+
+if __name__ == '__main__':
+    def test(status, ls):
+        pass
+
+    dc = {'QUERY_STRING': 'a=1&b=2&c=3'}
+    print(application(dc, test))
