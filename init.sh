b0812187adb5b732306a308bcf47716f8bb4f6ac
vasylrd <vasyl.rd@gmail.com>
2016-12-02 11:16:00 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
-sudo ln -sf /etc/nginx/sites-enabled/default /home/box/web/etc/default
+sudo ls -sf /etc/nginx/nginx.conf /home/box/web/etc/nginx.conf
+sudo nginx -t
 sudo /etc/init.d/nginx restart
-#sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn restart
-#sudo /etc/init.d/mysql start
+# sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+# sudo /etc/init.d/gunicorn restart
+# sudo /etc/init.d/mysql start
