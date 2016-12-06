198d2fe162379362aedc48290a9945aa21526dfe
vasylrd <vasyl.rd@gmail.com>
2016-12-06 13:31:17 +0200

    Update django_gunicorn_config.py

--- etc/django_gunicorn_config.py
+++ etc/django_gunicorn_config.py
@@ -1,15 +1,15 @@
-CONFIG = {                                                                                                   
-    'mode': 'wsgi',                                                                                       
-    'working_dir': '/home/box/web/ask',                                                                       
-    # 'python': '/usr/bin/python',                                                                           
-    'args': (                                                                                               
+CONFIG = {
+    'mode': 'wsgi',
+    'working_dir': '/home/box/web/ask',
+    'python': '/usr/bin/python3',
+    'args': (
         '--bind=0.0.0.0:8000',
         '--daemon',
-        '--workers=2',                                                                                     
+        '--workers=2',
         '--timeout=5',
-        '--access-logfile=/home/box/web/logs/gunicorn.access.log', 
+        '--access-logfile=/home/box/web/logs/gunicorn.access.log',
         '--error-logfile=/home/box/web/logs/gunicorn.error.log',
         '--log-level=info',
-        'ask.wsgi:application',                                                                                       
-    ),                                                                                                       
+        'ask.wsgi:application',
+    ),
 }
