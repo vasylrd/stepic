f9902806cf2fbc5a44c75caab17772968100e698
vasylrd <vasyl.rd@gmail.com>
2016-12-06 13:50:18 +0200

    Update init.sh

--- sh/init.sh
+++ sh/init.sh
@@ -1,12 +1,17 @@
 # Config and restart nginx
+
 sudo ln -sf /home/box/web/etc/default /etc/nginx/sites-enabled/default                              
 sudo nginx -t
 sudo /etc/init.d/nginx restart
-# run gunicorn
-sudo ln -sf /home/box/web/etc/django_gunicorn_config.py /etc/gunicorn.d/ask
-sudo /etc/init.d/gunicorn restart
 
 gunicorn -c /home/box/web/etc/hello_gunicorn_config.py hello:app --daemon
+gunicorn -c /home/box/web/etc/django_gunicorn_config.py wsgi --daemon
+
+# run gunicorn
+# sudo ln -sf /home/box/web/etc/django_gunicorn_config.py /etc/gunicorn.d/ask
+# sudo /etc/init.d/gunicorn restart
+
+# gunicorn -c /home/box/web/etc/hello_gunicorn_config.py hello:app --daemon
 # gunicorn -c /home/box/web/etc/django_gunicorn_config.py wsgi --daemon
 
 #cd /home/box/web && gunicorn -b 0.0.0.0:8080 hello:app &
