0449321a55afc3bebcd85b4d2348b1962b6b7630
vasylrd <vasyl.rd@gmail.com>
2016-12-06 12:40:13 +0200

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'qa.views.test'),
     url(r'^login/$', 'qa.views.test'),
     url(r'^singup/$', 'qa.views.test'),
     url(r'^question/(\d+)/$', 'qa.views.test'),
